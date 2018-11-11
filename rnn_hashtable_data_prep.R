ed6c9cc8abb18e5b7daec900d78efc40dbeed527
connermcb <connermcb@yahoo.com>
2018-11-10 23:13:56 -0600

    init commit alt data prep f() for data in hashtables

---
+++ rnn_hashtable_data_prep.R
@@ -0,0 +1,4 @@
+# This script is to prepare data that has already been organized into buckets by number of
+# tokens into a format that can be used by the bucketter function from the mxnet package and 
+# fed into the mxnet rnn model.
+
