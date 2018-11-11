24401ff2c856b3703d524418f7feb162094dda51
connermcb <connermcb@yahoo.com>
2018-11-11 12:58:31 -0600

    load data into matrix instead of dataframe

--- rnn_hashtable_data_prep.R
+++ rnn_hashtable_data_prep.R
@@ -12,35 +12,36 @@ prepareBatches <- function( hashtable, train_val_fraction=0.9 ){
         eval_buckets <- list()
 
         for( bkt in names(buckets)){
-                df <- as.data.frame(do.call(rbind, buckets[[bkt]]))
+
+                # created dataframe from list of vectors under bkt in env hashtable
+                mtrx <- do.call(rbind, buckets[[bkt]])
                 
+
                 # test that bucket actually has sentences
-                if( nrow(df) == 0 ) next
-                
-                X <- df[, 1:(as.integer(bkt) - 1)]
-                Y <- df[, 2:as.integer(bkt)]
-                
-                samples <- nrow(df)
-                
+                if( nrow(mtrx) == 0 ) next
+
+                X <- mtrx[, 1:(as.integer(bkt) - 1)]
+                Y <- mtrx[, 2:as.integer(bkt)]
+
+                samples <- nrow(mtrx)
+
                 X_train_data <- X[1:as.integer(samples * train_val_fraction), ]
                 X_val_data <- X[-(1:as.integer(samples * train_val_fraction)), ]
-                
+
                 X_train_label <- Y[1:as.integer(samples * train_val_fraction), ]
                 X_val_label <- Y[-(1:as.integer(samples * train_val_fraction)), ]
-                
-                train_buckets[bkt] <- list(data = X_train_data,
+
+                train_buckets[[bkt]] <- list(data = X_train_data,
                                            label = X_train_label)
-                
-                eval_buckets[bkt] <- list(data = X_val_data,
+
+                eval_buckets[[bkt]] <- list(data = X_val_data,
                                           label = X_val_label)
-                
-                print(length(train_buckets))
                 
         }
         return( list(train=train_buckets,
                      eval=eval_buckets
-                     ) 
+                     )
                 )
 }
 
-# prepareBatches("twitter_hashtable.RData")
+# test <- prepareBatches("twitter_hashtable.RData")
