26e87725a70c5e93226148d2910a8afa14c07b72
connermcb <connermcb@yahoo.com>
2018-11-10 23:09:44 -0600

    final ps version

--- sentence_bucketter.R
+++ sentence_bucketter.R
@@ -2,19 +2,18 @@
 
 # packages
 library(tidytext)
-library(tokenizers)
 library(readr)
 library(quanteda)
 
 # load sample data
-setwd("c:/users/conner/jhopkins_data_science/capstone/final/english_us/")
+setwd("c:/users/conner/jhopkins_data_science/capstone/final/")
 
 
 # function for bucketting sentences by number of tokens
 sentenceBucketter <- function(path){
         
         # read file
-        text_vec <- read_file(file = path)        
+        text_vec <- read_file(file = paste0("english_us/", path))        
         
         # split text file into sentences
         sent_vec <- tokens(text_vec, what = "sentence", remove_numbers = TRUE,
@@ -31,12 +30,14 @@ sentenceBucketter <- function(path){
                             remove_twitter = TRUE, remove_url = TRUE,
                             remove_symbols = TRUE, remove_separators = TRUE)
         
+        len_token_vec <- length(token_vec)
+        
+        # turn vector class from token to character
+        token_vec <- sapply(token_vec, as.character)
+        
         # clean up and make space in memory
         rm(sent_vec)
         gc()
-        
-        # create dictionary of unique tokens
-        dict <- c()
         
         # create hash table and and buckets named 1 to 100
         buckets <- new.env()
@@ -47,23 +48,28 @@ sentenceBucketter <- function(path){
         
         nxt_slot <- rep(1, 100)
         
+        # create progress bar
+        pb <- tkProgressBar(title = path, min = 0,
+                            max = len_token_vec, width = 300)
+
         # sort sentences (tokenized by word) to buckets 
         # based on number of words, i.e. sentence length
         system.time(
-        for(i in 1:100){
-                if (i %% 10 == 0){
-                        msg <- paste0(i, "% of sentences complete.")
-                        print(msg)
-                }
+        for(i in 1:len_token_vec){
+
+                # update progress bar
+                setTkProgressBar(pb, i, label=paste( round((i/len_token_vec)*100, 2),
+                                                     "% done"))
                 #change class to character from token
-                sentence <- token_list[[i]]
-                
+                sentence <- token_vec[i]
+
                 # check spelling and remove tokens not in dictionary
+                sentence <- tolower(sentence)
                 spell_check <- hunspell_check(sentence)
-                sentence <- tolower(sentence[spell_check])
+                sentence <- sentence[spell_check]
                 
+                # filter very short or very long sentences
                 sent_len <- length(sentence)
-                
                 if(sent_len < 4 | sent_len > 100) next
                 
                 # throw sentence in bucket
@@ -75,7 +81,21 @@ sentenceBucketter <- function(path){
         
         # name and save environment for use in 
         hashtable_name <- paste0(sub("en_US_([a-z0-9]+)\\.txt", "\\1", path), "_hashtable.RData")
+        cat("Saving ", hashtable_name, "\n")
         save(buckets, file = hashtable_name)
+        
+        # clean up memory
+        rm(token_vec)
+        rm(buckets)
+        gc()
+        
+        # clean up progress bar
+        close(pb)
 }
 
 paths <- c("en_US_blogs.txt", "en_US_news2.txt", "en_US_twitter.txt")
+
+for( p in paths[1:2] ){
+        sentenceBucketter(p)
+}
+
